0a980c829d2665ed6379158745975d45dae3b75f
1243428 <a1243428@01HW849271.local>
2017-11-27 19:43:09 +0530

    yeomen implementation

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'port': process.env.PORT || 8050,
+	//'database': 'mongodb://killer:drama@ds115166.mlab.com:15166/usercrm',
+	'database': 'mongodb://satish:satish@ds115166.mlab.com:15166/users',
+		//'database': 'mongodb://localhost:27017/testdb',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
